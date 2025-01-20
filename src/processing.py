9d62c84af649a0366ef65bbd260dd08af2c5e6dc
GrMorality <jmihailys@gmail.com>
2025-01-20 21:14:24 +1000

    added def filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,28 @@
+data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+def filter_by_state(data, state_filter):
+    filtered_data = [item for item in data if item['state'] == state_filter]
+    return {'data': filtered_data, 'filter': state_filter}
+
+
+# Получаем ввод пользователя
+user_input = input("Введите 'E' для EXECUTED или 'C' для CANCELED: ").upper()
+
+# Определяем state_filter на основе ввода
+if user_input == 'E':
+    state_filter = 'EXECUTED'
+elif user_input == 'C':
+    state_filter = 'CANCELED'
+else:
+    print("Некорректный ввод. Пожалуйста, введите 'E' или 'C'.")
+    exit()
+
+# Фильтруем и выводим результат
+result = filter_by_state(data, state_filter)
+print(f"Отфильтрованные данные для состояния '{result['filter']}':")
+for item in result['data']:
+    print(item)
