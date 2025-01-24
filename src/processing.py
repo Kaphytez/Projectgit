4405b35ebbb5be5b6af624563d5d4beb75d3e933
GrMorality <jmihailys@gmail.com>
2025-01-24 19:20:20 +1000

    updated function filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,33 +1,37 @@
 from datetime import datetime
 
-data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
 
 
-def filter_by_state(data, state_filter):
-    filtered_data = [stat for stat in data if stat['state'] == state_filter]
-    return {'data': filtered_data, 'filter': state_filter}
+def filter_by_state(data, state='EXECUTED'):
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+    Args:
+        data (list of dict): Список словарей для фильтрации.
+        state (str, optional): Значение ключа 'state' для фильтрации.
+                                По умолчанию 'EXECUTED'.
+    Returns:
+        list of dict: Список словарей, соответствующих заданному state.
+    """
+    filtered_data = [item for item in data if item.get('state') == state]
+    return filtered_data
 
 
-# Получаем ввод пользователя
-user_input = input(str("Введите 'E' для EXECUTED или 'C' для CANCELED: ")).upper()
+if __name__ == '__main__':
 
-# Определяем state_filter на основе ввода
-if user_input == 'E':
-    state_filter = 'EXECUTED'
-elif user_input == 'C':
-    state_filter = 'CANCELED'
-else:
-    print("Некорректный ввод. Пожалуйста, введите 'E' или 'C'.")
-    exit()
+    user_input = input("Введите 'E' для EXECUTED или 'C' для CANCELED: ").upper()
 
-# Фильтруем и выводим результат
-result = filter_by_state(data, state_filter)
-print(f"Отфильтрованные данные для состояния '{result['filter']}':")
-for stat in result['data']:
-    print(stat)
+    if user_input == 'C':
+        canceled_filter = filter_by_state(data, state='CANCELED')
+        print("'CANCELED':", canceled_filter)
+    else:
+        executed_filter = filter_by_state(data, state='EXECUTED')
+        print("'EXECUTED':", executed_filter)
 
 
 def sort_by_date(data, order):
