67be250a9cbe9f84d56d4f766301024a4e271ec7
GrMorality <jmihailys@gmail.com>
2025-01-26 23:47:52 +1000

    updated src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(account_info):
+def mask_account_card(account_info: str) -> str:
     """
     Маскирует номер карты или счета в строке, содержащей тип и номер.
     """
