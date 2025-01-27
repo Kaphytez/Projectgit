ad472f81f352754864ae42472eba15857a79b05c
GrMorality <jmihailys@gmail.com>
2025-01-27 19:59:55 +1000

    Now there is a main() function with the construction if __name__ == '__main__':. Added feature testing.

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from src.processing import data, filter_by_state, sort_by_date
 
-another_action = input("Хотите ли вы отсортировать 'data' или отфильтровать 'state'? (да/нет): ").lower()
 
-if another_action == 'да':
-    choice = input("Выберите действие: 'f' для фильтрации по 'state', 's' для сортировки по 'date': ").lower()
-    if choice == 'f':
-        state_filter = input("Введите значение 'state' для фильтрации ('EXECUTED', 'CANCELED'): ").strip()
-        if state_filter:
-            filtered_data = filter_by_state(data, state=state_filter)
-            print(filtered_data)
+def main_function(another_action, choice=None, state_filter=None, order=None):
+    if another_action == 'да':
+        if choice == 'f':
+            if state_filter:
+                filtered_data = filter_by_state(data, state=state_filter)
+            else:
+                filtered_data = filter_by_state(data, state="EXECUTED")
+            return filtered_data
+
+        elif choice == 's':
+            if order == 'a':
+                sorted_data = sort_by_date(data)
+            elif order == 'd':
+                sorted_data = sort_by_date(data, ascending=False)
+            else:
+                sorted_data = sort_by_date(data)
+            return sorted_data
         else:
-            filtered_data = filter_by_state(data, state="EXECUTED")
-            print(filtered_data)
-    elif choice == 's':
-        order = input("Введите 'a' для сортировки по возрастанию или 'd' для сортировки по убыванию: ").lower()
-        if order == 'a':
-            sorted_data = sort_by_date(data)
-        elif order == 'd':
-            sorted_data = sort_by_date(data, ascending=False)
-        else:
-            print('Неверный ввод. Сортировка по умолчанию - по возрастанию')
-            sorted_data = sort_by_date(data)
-        print(sorted_data)
-    else:
-        print("Программа завершена.")
+            return "Программа завершена."
+    return None  # Если another_action не равен 'да'
+
+
+if __name__ == "__main__":
+    another_action = input("Хотите ли вы отсортировать 'data' или отфильтровать 'state'? (да/нет): ").lower()
+    choice = None
+    state_filter = None
+    order = None
+    if another_action == 'да':
+        choice = input("Выберите действие: 'f' для фильтрации по 'state', 's' для сортировки по 'date': ").lower()
+        if choice == 'f':
+            state_filter = input("Введите значение 'state' для фильтрации ('EXECUTED', 'CANCELED'): ").strip()
+        elif choice == 's':
+            order = input("Введите 'a' для сортировки по возрастанию или 'd' для сортировки по убыванию: ").lower()
+
+    result = main_function(another_action, choice, state_filter, order)
+    print(result)
---
+++ tests/test_main.py
@@ -0,0 +1,63 @@
+import pytest
+
+from main import main_function
+
+
+def test_main_function_filter_executed():
+    expected = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
+
+    assert main_function(another_action="да", choice="f", state_filter="EXECUTED") == expected
+
+
+def test_main_function_filter_canceled():
+    expected = [
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+    assert main_function(another_action="да", choice="f", state_filter="CANCELED") == expected
+
+
+def test_main_function_filter_default():
+    expected = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
+    assert main_function(another_action="да", choice="f", state_filter=None) == expected
+
+
+def test_main_function_sort_asc():
+    expected = [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+    ]
+
+    assert main_function(another_action="да", choice="s", order="a") == expected
+
+
+def test_main_function_sort_desc():
+    expected = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
+    assert main_function(another_action="да", choice="s", order="d") == expected
+
+
+def test_main_function_sort_default():
+    expected = [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+    ]
+    assert main_function(another_action="да", choice="s", order=None) == expected
+
+
+def test_main_function_no_action():
+    assert main_function(another_action="нет") is None
